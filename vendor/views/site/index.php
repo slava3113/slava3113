8c7696b83da7fca7050e0131b723e279caea16d5
slava3113 <slava3113@ramler.ru>
2022-01-22 11:10:17 +0600

    First index file

---
+++ vendor/views/site/index.php
@@ -0,0 +1,3 @@
+<h1>Video Shcoll</h1>
+
+welcome to the Video Paradise :)
--- web/index.php
+++ web/index.php
@@ -1,1 +1,16 @@
-Hello 
+<?php
+define('YII_DEBUG', true);
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+$config =  [
+  'id' => 'video',
+  'basePath' => realpath(__DIR__ . '/../')
+];
+$yii = new yii\web\Application($config);
+$yii->run ();
+
+
+
+
+
+
+
